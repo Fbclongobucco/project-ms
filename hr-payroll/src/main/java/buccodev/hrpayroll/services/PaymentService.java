db02e08e3328d5ef84ce1f9625281d5f24ccacf5
Fbclongobucco <longobuccofbc@gmail.com>
2023-09-10 22:33:46 -0300

    Feign client

--- hr-payroll/src/main/java/buccodev/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/buccodev/hrpayroll/services/PaymentService.java
@@ -1,30 +1,23 @@
 package buccodev.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import buccodev.hrpayroll.entities.Payment;
 import buccodev.hrpayroll.entities.Worker;
+import buccodev.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariabels = new HashMap<>();
-		uriVariabels.put("id", ""+workerId);
 		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}",Worker.class, uriVariabels );
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
